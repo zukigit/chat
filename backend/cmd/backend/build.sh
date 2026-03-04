c389d0076b1a5c81a8940f8747aaa60ac6bf9f27
zuki@rocky10 <zukidono@gmail.com>
2026-03-04 23:53:21 +0700

    Refactor: Streamline binary build and deployment by removing intermediate `bin/` directory and artifact steps.

---
+++ backend/cmd/backend/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "Building backend binary..."
+docker run --rm \
+    -v "$(pwd)":/workspace \
+    -w /workspace golang:1.25.3-alpine3.22 sh \
+    -c "go build -o chat_backend github.com/zukigit/chat/backend/cmd/backend"
+echo "Backend binary built successfully."
+
