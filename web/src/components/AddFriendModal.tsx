12c10143350d373d5a9f0589b068d1d822699d77
zuki <zukidono@gmail.com>
2026-04-20 22:53:58 +0700

    removed unused callback

--- web/src/components/AddFriendModal.tsx
+++ web/src/components/AddFriendModal.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef } from 'react'
+import { useState, useRef } from 'react'
 import './chat.css'
 import { avatarColor, avatarInitials } from './avatarUtils'
 import { getToken } from '../auth'
